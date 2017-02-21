11211f947ee853f6dcf54dcf1033333798830e79
Andy Denler <andy.denler@lextech.com>
2017-02-21 09:07:56 -0500

    feat(departments): add insert department

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,5 +1,6 @@
+import { Department } from './../../shared/models/department.model';
 import { Component, OnInit } from '@angular/core';
-import { Apollo } from 'apollo-angular';
+import { Apollo, ApolloQueryObservable } from 'apollo-angular';
 import gql from 'graphql-tag';
 import { Observable } from 'rxjs/Observable';
 
@@ -13,27 +14,68 @@ const departmentsQry = gql`
   }
 `;
 
+const departmentAddQry = gql`
+  mutation ($dept: DepartmentInput!) {
+    insertUpdateDepartment (department: $dept) {
+      dept_no
+      dept_name
+    }
+  }
+`;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  departmentSubscription: ApolloQueryObservable<any>;
+
   departments: Array<any>;
+  dept_no = '';
+  dept_name = '';
+
   constructor(
     private apollo: Apollo
   ) { }
 
   ngOnInit() {
-    this.apollo.watchQuery({
+    this.departmentSubscription = this.apollo.watchQuery({
       query: departmentsQry
-    }).subscribe(({data}) => {
+    });
+
+    this.departmentSubscription.subscribe(({data}) => {
+      console.log('department data loaded');
       this.departments = (<any>data).departments;
     });
   }
 
   addDepartment() {
-    // this.apollo.mutate()
+    if (this.dept_no && this.dept_name) {
+      this.apollo.mutate({
+        mutation: departmentAddQry,
+        variables: {
+          dept: {
+            dept_no: this.dept_no,
+            dept_name: this.dept_name
+          }
+        },
+        updateQueries: {
+          Departments: (prev, {mutationResult}: any) => {
+
+            return {
+              departments: [mutationResult.data.insertUpdateDepartment, ...(<any>prev).departments]
+            };
+          }
+        }
+      }).subscribe(
+        ({ data }) => {
+          console.log('got data', data);
+        }, (err) => {
+          console.log('there was an error sending the query', err);
+        });
+
+    }
   }
 
 }
